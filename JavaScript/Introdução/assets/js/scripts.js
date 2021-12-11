185942584ba9d9b1cd1703be1f0849313d043600
CarlosPetito <junior.petito@gmail.com>
2021-12-11 15:55:52 -0300

    Adicionando de 10 até -10, número negativo vermelho e usando addEventListener

--- JavaScript/Introdução/assets/js/scripts.js
+++ JavaScript/Introdução/assets/js/scripts.js
@@ -1,12 +1,27 @@
-var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0;
 
-function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+document.getElementById("subtrair").addEventListener("click", function(){
+    if (currentNumber >= -9) {
+        currentNumber = currentNumber - 1;
+        document.getElementById("currentNumber").innerHTML = currentNumber;
+    } else {}
+});
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+document.getElementById("adicionar").addEventListener("click", function(){
+    if (currentNumber <= 9) {
+        currentNumber = currentNumber + 1;
+        document.getElementById("currentNumber").innerHTML = currentNumber;
+    } else {}
+});
+
+function red(){
+    if (currentNumber <= 0) {
+    document.getElementById("currentNumber").classList.add("negativo");
+    }
+};
+
+function black(){
+    if (currentNumber >= -1) {
+        document.getElementById("currentNumber").classList.remove("negativo");
+    }
+};
